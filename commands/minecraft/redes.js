ff3a35ede7a398d039b31f0cb18ea3d25822d39d
Santiago Forero <thesantiagoforero@gmail.com>
2021-02-19 17:41:17 -0500

    update links

--- commands/minecraft/redes.js
+++ commands/minecraft/redes.js
@@ -13,7 +13,7 @@ module.exports = {
         embed
             .setTitle('Redes Sociales')
             .setColor(randomColor())
-            .setDescription(`<:instagram:812447736078663720> [Instagram](instagram.com/minecraft_un/)\n <:telegram:812447684132339733> [Telegram](https://t.me/unminecraft)\n <:discord:812448001275592714> [Discord](https://discord.gg/9NtGSAyDV9)`)
+            .setDescription(`<:instagram:812447736078663720> [Instagram](https://instagram.com/minecraft_un/)\n <:telegram:812447684132339733> [Telegram](https://t.me/unminecraft)\n <:discord:812448001275592714> [Discord](https://discord.gg/9NtGSAyDV9)`)
         
 
         message.channel.send(embed);
