5d3bacd9b3721498416a0250813eba9d45368a5d
Santiago Forero <santiago@forero.ga>
2021-09-24 19:58:08 -0500

    create simple webserver on bot start and modify .env.example

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const { Client, Intents, Collection } = require('discord.js')
+const express = require('express')
 require('dotenv').config()
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
@@ -24,3 +25,10 @@ for (const file of commandFiles) {
 }
 
 client.login(process.env.TOKEN)
+
+const app = express()
+app.get('/', (req, res) => {
+  res.send(`Guilds: ${client.guilds.cache.size}, users: ${client.users.cache.size}`)
+})
+
+app.listen(process.env.PORT)
