187002ffcdc273e7e165bccc2807a4f0dbcbd6f5
Santiago Forero <santiago@forero.ga>
2021-09-29 12:22:43 -0500

    standard lint

--- commands/python.js
+++ commands/python.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const axios = require('axios')
 
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { MessageEmbed } = require('discord.js')
@@ -9,31 +9,27 @@ module.exports = {
     .setDescription('Evalua código de python.')
     .addStringOption(option => option.setName('code').setDescription('Código a evaluar.').setRequired(true)),
   async execute (interaction) {
-
     const input = interaction.options.getString('code')
-
 
     const embed = new MessageEmbed()
       .setColor('#ffd343;')
       .setTitle('Python')
       .setTimestamp()
 
-
     axios.get('http://dotpy3.herokuapp.com?' + input)
-    .then(function (response) {
+      .then(function (response) {
         embed.addFields(
-            { name: 'Código', value: '```py\n ' + input + '\n```' },
-        { name: '\u200B', value: '\u200B' },
-        { name: 'Salida', value: '```py\n ' + response.data + '\n```' },
+          { name: 'Código', value: '```py\n ' + input + '\n```' },
+          { name: '\u200B', value: '\u200B' },
+          { name: 'Salida', value: '```py\n ' + response.data + '\n```' }
         )
-    })
-    .catch(function (error) {
-      embed.setDescription('Hubo un error :warning:')
-      console.log(error);
-    })
-    .then(function () {
-        return interaction.reply({embeds: [embed]})
-    });
-
+      })
+      .catch(function (error) {
+        embed.setDescription('Hubo un error :warning:')
+        console.log(error)
+      })
+      .then(function () {
+        return interaction.reply({ embeds: [embed] })
+      })
   }
 }
